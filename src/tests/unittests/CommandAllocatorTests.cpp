729acd853a489b5457622feec34f46d011f252e7
Kai Ninomiya <kainino1@gmail.com>
2017-07-21 10:17:20 -0700

    bunch more warnings

--- src/tests/unittests/CommandAllocatorTests.cpp
+++ src/tests/unittests/CommandAllocatorTests.cpp
@@ -203,7 +203,7 @@ TEST(CommandAllocator, LargeCommands) {
 
     const int kCommandCount = 5;
 
-    int count = 0;
+    uint32_t count = 0;
     for (int i = 0; i < kCommandCount; i++) {
         CommandBig* big = allocator.Allocate<CommandBig>(CommandType::Big);
         for (int j = 0; j < kBigBufferSize; j++) {
